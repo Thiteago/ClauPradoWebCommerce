e214cb0c42fd50cf99f99f27b8ec0270a58ff334
Thiago <thiago_galaxy@hotmail.com>
2021-05-31 14:04:40 -0300

    Fix bug on hover and adjust Title size slider

--- js/slide.js
+++ js/slide.js
@@ -1,14 +1,20 @@
 function slide1(){
     document.getElementById('img').src="assets/img/produto-3.jpeg";
+    document.getElementById('titulo').innerHTML = 'Unicórnio Camarada';
+    document.getElementById('descricao').innerHTML = 'Era uma vez um unicórnio que se chamava rodolfo';
     setTimeout("slide2()", 6000)
 }
 
 function slide2(){
     document.getElementById('img').src="assets/img/produto-5.jpg";
+    document.getElementById('titulo').innerHTML = 'Leri go, Leri go';
+    document.getElementById('descricao').innerHTML = 'Lindas cantigas de ninar para crianças imperativas';
     setTimeout("slide3()", 6000)
 }    
 
 function slide3(){
     document.getElementById('img').src="assets/img/produto-2.jpeg";
+    document.getElementById('titulo').innerHTML = 'Harry Bruxo';
+    document.getElementById('descricao').innerHTML = 'Rasta a tabaca na vara vai sentando na vassoura , eta bruxinha rabuda , eta rabeta que voa';
     setTimeout("slide1()", 6000)
 }        
