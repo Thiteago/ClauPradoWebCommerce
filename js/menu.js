c5b178b954c7a691fbd0ea5f637abd5eda06531c
Thiago <thiagodavidpd@gmail.com>
2021-06-15 22:01:41 -0300

    responsive sections

--- js/menu.js
+++ js/menu.js
@@ -10,3 +10,10 @@ var trigger = document.getElementById('menu-trigger').addEventListener("click",f
         }
     }
 )
+
+new SimpleSlide({
+    slide: 'principal', // nome do atributo data-slide="principal"
+    nav: true, // se deve ou não mostrar a navegação
+    time: 5000, // tempo de transição dos slides
+    pauseOnHover: true, // pausa a transição automática
+  });
