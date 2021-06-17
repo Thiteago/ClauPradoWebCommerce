88e5710b6626a2317c3d31fb030a87b7fb2dbadb
Thiago <thiago_galaxy@hotmail.com>
2021-06-17 10:22:39 -0300

    Finish Index.html responsivity

--- js/script.js
+++ js/script.js
@@ -3,22 +3,21 @@ $(".slider").owlCarousel({
     singleItem: true,
     loop: true,
     autoplay:true,
-    autoplayTimeout: 10000,
+    autoplayTimeout: 5000,
     items:1,
     responsiveClass:true,
     responsive:{
         0:{
             items:1,
-            nav:true
+            nav:false
         },
         600:{
             items:3,
             nav:false
         },
         1000:{
-            items:4,
+            items:3,
             nav:true,
-            loop:false
         }
     }
 });
